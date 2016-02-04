9c76343540696c63b5113d6d9afa3dce4fbc0b27
suzie(수지) <suz@coupang.com>
2016-02-04 17:29:23 +0900

    get book info

--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,32 @@
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+function renderUserInfo(userEmail) {
+  document.getElementById('user_email').textContent = "id : " + userEmail;
 }
 
 function renderPrice(priceText) {
-	document.getElementById('price').textContent = priceText;
+	document.getElementById('price').textContent = "price: " + priceText;
+}
+
+function renderBookName(bookText) {
+    document.getElementById('book_name').textContent = "book name : " + bookText;
 }
 	
+function renderISBN13(isbn13) {
+    document.getElementById('isbn13').textContent = "isbn :" + isbn13;
+}
+
+function renderUrl(url) {
+    document.getElementById('url').textContent = "url : " + url;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	chrome.identity.getProfileUserInfo(function(userInfo) {
-		renderStatus(userInfo.email);
+		renderUserInfo(userInfo.email);
 	});
 });
 
 function onWindowLoad() {
     chrome.tabs.executeScript(null, {
-        file: "js/getPrice.js"
+        file: "js/getBookInfo.js"
         }, function() {
             if (chrome.extension.lastError) {
                 document.body.innerText = 'There was an error injecting script : \n' + chrome.extension.lastError.message;
@@ -27,5 +39,23 @@ window.onload = onWindowLoad;
 chrome.extension.onMessage.addListener(function(request, sender) {
     if (request.action == "getPrice") {
     	renderPrice(request.source);
+    }
+});
+
+chrome.extension.onMessage.addListener(function(request, sender) {
+    if (request.action == "getBookName") {
+        renderBookName(request.source);
+    }
+});
+
+chrome.extension.onMessage.addListener(function(request, sender) {
+    if (request.action == "getUrl") {
+        renderUrl(request.source);
+    }
+});
+
+chrome.extension.onMessage.addListener(function(request, sender) {
+    if (request.action == "getISBN13") {
+        renderISBN13(request.source);
     }
 });
