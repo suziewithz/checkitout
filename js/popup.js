14f9a014835567dc9e26915711080800b09a2b66
neo(배성열) <bsr117@coupang.com>
2016-02-11 01:27:06 +0900

    popup.html에 cart 추가 코드 및 hidden input 추가 git ignore 추가도...

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,28 @@
 function renderUserInfo(userEmail) {
-  document.getElementById('user_email').textContent = "id : " + userEmail;
+    $('#user_email').text("id : " + userEmail);
+    $('input[name="input_user_email"]').val(userEmail);
 }
 
 function renderPrice(priceText) {
-	document.getElementById('price').textContent = "price: " + priceText;
+	$('#price').text("price: " + priceText + "원")
+    afterReplace = priceText.replace(/,/g,"");
+    afterReplace = afterReplace.replace(/원/g, "");
+    $('input[name="input_price"]').val(afterReplace);
 }
 
 function renderBookName(bookText) {
-    document.getElementById('book_name').textContent = "book name : " + bookText;
+    $('#book_name').text("book name : " + bookText);
+    $('input[name="input_book_name"]').val(bookText);
 }
 	
 function renderISBN13(isbn13) {
-    document.getElementById('isbn13').textContent = "isbn :" + isbn13;
+    $('#isbn13').text("isbn :" + isbn13);
+    $('input[name="input_isbn13"]').val(isbn13);
 }
 
 function renderUrl(url) {
-    document.getElementById('url').textContent = "url : " + url;
+    $('#url').text("url : " + url);
+    $('input[name="input_url"]').val(url);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -25,6 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function onWindowLoad() {
+    chrome.tabs.executeScript(null, {file:"js/jquery.min.js"}, null);
     chrome.tabs.executeScript(null, {
         file: "js/getBookInfo.js"
         }, function() {
@@ -58,4 +66,38 @@ chrome.extension.onMessage.addListener(function(request, sender) {
     if (request.action == "getISBN13") {
         renderISBN13(request.source);
     }
+});
+
+
+$(document).ready( function() {
+    
+    $('button#addCart').click(function() {
+        cookieBook = {};
+        cookieBook.name = $('input[name="input_book_name"]').val();
+        cookieBook.price = $('input[name="input_price"]').val();
+        cookieBook.isbn13 = $('input[name="input_isbn13"]').val();
+        cookieBook.url = $('input[name="input_url"]').val();
+        isNeedToAdd = true;
+
+        chrome.cookies.get({"name":"cartList", "url":"https://www.yes24.com/"}, function(cookie) {
+            if(cookie != null) {
+                cookieBookList = JSON.parse(cookie.value);
+            } else {
+                cookieBookList = [];
+            }
+            
+            for(i = 0 , len = cookieBookList.length; i < len; ++i) {
+                if(cookieBookList[i].isbn13 == cookieBook.isbn13) {
+                    isNeedToAdd = false;
+                    console.log("already added");
+                    break;
+                }
+            }
+
+            if(isNeedToAdd) {
+                cookieBookList.push(cookieBook);
+                chrome.cookies.set({"name": "cartList", "url": "https://www.yes24.com/", "value": JSON.stringify(cookieBookList)}, function (cookie) {});
+            }
+        });
+    });
 });
