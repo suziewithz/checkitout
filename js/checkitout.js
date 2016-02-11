894ebcd181c139c4fc21ebb0599f4a664e368aed
andew(주영광) <andew.ju@coupang.com>
2016-02-11 10:24:30 +0900

    merge

--- js/checkitout.js
+++ js/checkitout.js
@@ -1,11 +1,13 @@
 var checkitout = {
 
 	init: function(){
-
+		checkitout.addEvent();
 	},
 
 	addEvent: function(){
-
+		//$('#test').click(function() {
+		//	checkitout.testpopup();
+		//});
 	},
 
     getProduct: function(category, productName) {
@@ -32,6 +34,19 @@ var checkitout = {
             }
         });
     },
+
+	testpopup: function(){
+
+		//chrome.browserAction.onClicked.addListener(function() {
+		//	var w = 440;
+		//	var h = 220;
+		//	var left = (screen.width/2)-(w/2);
+		//	var top = (screen.height/2)-(h/2);
+		//
+		//	chrome.windows.create({'url': 'http://local.coupang.com:9999/login_google', 'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
+		//	});
+		//});
+	}
 }
 
 $(document).ready(function() {
