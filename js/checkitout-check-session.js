8f06de2275e39a707752119a70fd034e0dac5842
andew(주영광) <andew.ju@coupang.com>
2016-02-11 15:37:00 +0900

    Impi login

---
+++ js/checkitout-check-session.js
@@ -0,0 +1,34 @@
+checkitout.check_session = {
+	jsseionId : '',
+	baseUrl : 'http://local.coupang.com:9999',
+	init: function(){
+		checkitout.check_session.checkSession();
+	},
+
+	checkSession: function() {
+		$.ajax({
+			url: checkitout.check_session.baseUrl + '/userinfo',
+			method: "POST",
+			success: function (result, status, xhr) {
+				if (xhr.status == 200) {
+					if (result != null) {
+						var rcode = result.rcode;
+						if(rcode == 'RET0000'){
+							$(location).attr('href', '/html/default.html');
+						}
+						else{
+							$(location).attr('href', '/html/signin.html');
+						}
+					}
+				}
+			},
+			error: function () {
+
+			}
+		});
+	}
+}
+
+$(document).ready(function() {
+	checkitout.check_session.init();
+});
---
+++ js/checkitout-signin.js
@@ -0,0 +1,42 @@
+checkitout.signin = {
+	jsseionId : '',
+	baseUrl : 'http://local.coupang.com:9999',
+	init: function(){
+		checkitout.signin.addEvent();
+	},
+
+	addEvent: function(){
+		//$('#test').click(function() {
+		//	checkitout.testpopup();
+		//});
+	},
+
+    test: function() {
+        $.ajax({
+            url: checkitout.signin.baseUrl + '/index',
+            method: "GET",
+            //data: { category: category, productName: productName },
+            success: function (result, status, xhr) {
+                 var ct = xhr.getResponseHeader("content-type") || "";
+                 if (ct.indexOf('html') > -1) {
+                     //Sesseion exfired, Redirect login page
+                     $(location).attr('href', 'http://' + window.location.host + '/login');
+                 }
+                 else if (xhr.status == 200) {
+                     //GET Ok
+                     if (result != null) {
+                         //coukie.index.appendRow(result);
+                     }
+                 }
+            },
+            error: function () {
+                //$(location).attr('href','http://'+window.location.host+'/index');
+            }
+        });
+    },
+
+}
+
+$(document).ready(function() {
+	checkitout.signin.init();
+});
