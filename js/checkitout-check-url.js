409c2a10fe9b94fcc94a9b083f62572f5e8d3065
gloryic <andew.ju@coupang.com>
2016-02-15 13:28:38 +0900

    [hot fix] modify mld-card height

--- js/checkitout-check-url.js
+++ js/checkitout-check-url.js
@@ -14,7 +14,7 @@ checkitout.checkUrl = {
 			$contents.empty();
 			var tmp =
 				'<div class="login-box is-active" id="overview">'
-				+ '<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">'
+				+ '<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp mdl-link-card">'
 				+ '<div class="mdl-card mdl-cell mdl-cell--12-col">'
 				+ '<div class="mdl-card__supporting-text">'
 				+ '<h4>Info</h4>'
